7a3da20347d3ac37507c3ec2e0b697d1fa6297ce
Aaron Pickering <aaronpickering@TD-00491-mac.fritz.box>
2020-07-13 23:27:09 +0200

    Added 1st and 2nd lags for media.
    Added  7th of July variable.
    
    Experimented with manual structural break and multiplicative seasonality.

--- python/fbprophet/fbprophet.py
+++ python/fbprophet/fbprophet.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import datascroller as scroller
 import sys
 import os
-import statsmodels
+import statsmodels.tsa.stattools as sm
 import generate_time_series as gts
 
 # Import time series generating functions
@@ -18,7 +18,23 @@ sample_data.columns = ['ds','media','y']
 #  Convert the date object to datetime
 sample_data['ds'] = pd.to_datetime(sample_data['ds'])
 
+# Add a media variable at lag 1 and 2
 sample_data['media_lag_1'] = sample_data['media'].shift(1)
+sample_data['media_lag_2'] = sample_data['media'].shift(2)
+
+# Add a 31 day lag variable
+sample_data['y_lag_31'] = sample_data['y'].shift(31)
+
+# Fill NA values with zeroes
+sample_data.loc[0,'media_lag_1'] = 0
+sample_data.loc[0:1,'media_lag_2'] = 0
+
+# Fill the y_lag_31 values with the first genuine data point
+sample_data.loc[0:30,'y_lag_31'] = 6.258472
+
+# Add a 7th of July dummy to account for the negative spike
+sample_data['seven_seven'] = sample_data['ds'].isin(["2018-07-07","2019-07-07","2020-07-07","2021-07-07"])
+sample_data['seven_seven'] = sample_data['seven_seven'].astype(int)
 
 # Split into training and test sets
 train_indices = np.where(~(sample_data['y'].isnull()))[-1]
@@ -26,6 +42,10 @@ test_indices = np.where(sample_data['y'].isnull())[-1]
 
 train = sample_data.iloc[train_indices,:]
 test = sample_data.iloc[test_indices,:]
+
+# Drop the na values from the lagged terms
+train = train.dropna()
+
 
 #-------- Exploratory analysis
 # Plot the sample Sales over time
@@ -38,18 +58,18 @@ plt.show()
 
 # Non-stationarity tests (ADF, KPSS) - eyeball it to start with
 # ACF - Clearly autocorrelation
-acf_values = statsmodels.tsa.stattools.acf(train['y'])
+acf_values = sm.acf(train['y'])
 plt.plot(acf_values)
 plt.show()
 # PACF
-acf_values = statsmodels.tsa.stattools.pacf(train['y'])
+acf_values = sm.pacf(train['y'])
 plt.plot(acf_values)
 plt.show()
 
 # Trend, seasonality remainder.
 # What is happening in January 2020?
-# Media Cross correlation
-statsmodels.tsa.stattools.ccf(train['media'], train['y'])
+# Media Cross correlation (after removing seasonality)
+sm.ccf(train['media'], train['y'])
 
 
 # Calculate the errors - Error (MAPE?)
@@ -77,8 +97,8 @@ forecast_plot.show()
 comp_plot = prophet_model.plot_components(fit)
 comp_plot.show()
 
-# Add the external regressor (Media) and retrain
-prophet_model = fbprophet.Prophet()
+# Add the external regressor (Media) and retrain. Disable weekly seasonality.
+prophet_model = fbprophet.Prophet(weekly_seasonality=False)
 prophet_model.add_regressor("media")
 prophet_model.fit(train)
 
@@ -87,6 +107,11 @@ fit_x = prophet_model.predict(train)
 # Plot the new fit
 fit_plot = prophet_model.plot(fit_x)
 fit_plot.show()
+
+plt.plot(train['y'])
+plt.plot(fit_x['yhat'])
+plt.show()
+
 
 # Lets look  at the changepoint detection
 fit_plot = prophet_model.plot(fit_x)
@@ -102,8 +127,10 @@ plt.plot(train['ds'], res)
 plt.show()
 
 # Play around with change point prior
-prophet_model = fbprophet.Prophet(changepoint_prior_scale=0.2)
+prophet_model = fbprophet.Prophet(changepoint_prior_scale=0.2, weekly_seasonality=False)
 prophet_model.add_regressor("media")
+prophet_model.add_regressor("media_lag_1")
+prophet_model.add_regressor("media_lag_2")
 fit_xc = prophet_model.fit(train)
 
 fit_xc = fit_xc.predict(train)
@@ -112,14 +139,22 @@ a = fbprophet.plot.add_changepoints_to_plot(fit_plot.gca(), prophet_model, fit_x
 fit_plot.show()
 
 #  Seems to be a discontinuity at 2020-01.  Lets specifiy a change point at 2020-01 and assume liner otherwise.
-prophet_model = fbprophet.Prophet(changepoints=["2020-01-01","2020-01-25"])
+prophet_model = fbprophet.Prophet(changepoints=["2019-12-15","2020-03-01"], weekly_seasonality=False)
+prophet_model.add_seasonality("yearly", period=365.25, fourier_order=12, mode="multiplicative")
 prophet_model.add_regressor("media")
+prophet_model.add_regressor("media_lag_1")
+prophet_model.add_regressor("media_lag_2")
+prophet_model.add_regressor("media_lag_1")
+prophet_model.add_regressor("seven_seven")
 fit_xc = prophet_model.fit(train)
 
 fit_xc = fit_xc.predict(train)
 fit_plot = prophet_model.plot(fit_xc)
 a = fbprophet.plot.add_changepoints_to_plot(fit_plot.gca(), prophet_model, fit_xc)
 fit_plot.show()
+
+plt.plot(fit_xc['multiplicative_terms'])
+plt.show()
 
 # Plot the residuals again
 res = train['y'] - fit_xc['yhat']
@@ -142,7 +177,9 @@ prophet_model = fbprophet.Prophet(seasonality_mode="multiplicative", changepoint
 prophet_model.add_regressor("media")
 prophet_model.add_regressor("struct_break")
 prophet_model.add_regressor("media_lag_1")
-prophet_model.add_seasonality('yearly', period=365.25, fourier_order=6, mode='multiplicative')
+prophet_model.add_regressor("y_lag_31")
+prophet_model.add_seasonality('yearly', period=365.25, fourier_order=12, mode='multiplicative')
+prophet_model.add_seasonality('yearly', period=365.25, fourier_order=12, mode='additive')
 train = train.dropna()
 fit_xc = prophet_model.fit(train)
 
@@ -151,6 +188,8 @@ fit_plot = prophet_model.plot(fit_xc)
 a = fbprophet.plot.add_changepoints_to_plot(fit_plot.gca(), prophet_model, fit_xc)
 fit_plot.show()
 
+fig1 = prophet_model.plot_components(fit_xc)
+fig1.show()
 
 
 # Plot the residuals again

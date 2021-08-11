2e60c262bc57433ffc1c80c56862f1bf5a9d4306
Juan Orduz <juanitorduz@gmail.com>
2021-08-11 15:02:28 +0200

    initial model plots

---
+++ python/intro_forecasting/mapping.py
@@ -0,0 +1,71 @@
+PARAMS = {
+    'irregular': {
+        'zeta': 0,
+        'beta': 0,
+        'beta_1': 0,
+        'eta': 0,
+        'epsilon': 1,
+        'mu_1': 0,
+        'mu': 0,
+    },
+
+    'fixed intercept': {
+        'zeta': 0,
+        'beta': 0,
+        'beta_1': 0,
+        'eta': 0,
+        'epsilon': 0,
+        'mu_1': 0,
+        'mu': 1,
+    },
+
+    'local level' : {
+        'zeta': 0,
+        'beta': 0,
+        'beta_1': 0,
+        'eta': 1,
+        'epsilon': 1,
+        'mu_1': 1,
+        'mu': 1,
+    },
+
+    'random walk' : {
+        'zeta': 0,
+        'beta': 0,
+        'beta_1': 0,
+        'eta': 0,
+        'epsilon': 1,
+        'mu_1': 1,
+        'mu': 1,
+    },
+
+    'fixed slope' : {
+        'zeta': 0,
+        'beta': 0,
+        'beta_1': 0,
+        'eta': 0,
+        'epsilon': 0,
+        'mu_1': 1,
+        'mu': 1,
+    },
+
+    'local linear trend' : {
+        'zeta': 1,
+        'beta': 0,
+        'beta_1': 1,
+        'eta': 1,
+        'epsilon': 1,
+        'mu_1': 1,
+        'mu': 1,
+    },
+
+    'smooth trend' : {
+        'zeta': 1,
+        'beta': 0,
+        'beta_1': 1,
+        'eta': 0,
+        'epsilon': 1,
+        'mu_1': 1,
+        'mu': 1,
+    }
+}

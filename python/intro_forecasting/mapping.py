7dc3c698049c5b99f29f04821249986211111146
juanitorduz <juanitorduz@gmail.com>
2021-08-23 12:42:23 +0200

    correct mapping parameters

--- python/intro_forecasting/mapping.py
+++ python/intro_forecasting/mapping.py
@@ -19,37 +19,37 @@ PARAMS = {
         'mu': 1,
     },
 
-    'local level' : {
+    'random walk': {
         'zeta': 0,
         'beta': 0,
         'beta_1': 0,
         'eta': 1,
-        'epsilon': 1,
-        'mu_1': 1,
-        'mu': 1,
-    },
-
-    'random walk' : {
-        'zeta': 0,
-        'beta': 0,
-        'beta_1': 0,
-        'eta': 0,
-        'epsilon': 1,
-        'mu_1': 1,
-        'mu': 1,
-    },
-
-    'fixed slope' : {
-        'zeta': 0,
-        'beta': 0,
-        'beta_1': 0,
-        'eta': 0,
         'epsilon': 0,
         'mu_1': 1,
         'mu': 1,
     },
 
-    'local linear trend' : {
+    'local level': {
+        'zeta': 0,
+        'beta': 0,
+        'beta_1': 0,
+        'eta': 1,
+        'epsilon': 1,
+        'mu_1': 1,
+        'mu': 1,
+    },
+
+    'smooth trend': {
+        'zeta': 1,
+        'beta': 0,
+        'beta_1': 1,
+        'eta': 0,
+        'epsilon': 1,
+        'mu_1': 1,
+        'mu': 1,
+    },
+
+    'local linear trend': {
         'zeta': 1,
         'beta': 0,
         'beta_1': 1,
@@ -59,13 +59,4 @@ PARAMS = {
         'mu': 1,
     },
 
-    'smooth trend' : {
-        'zeta': 1,
-        'beta': 0,
-        'beta_1': 1,
-        'eta': 0,
-        'epsilon': 1,
-        'mu_1': 1,
-        'mu': 1,
-    }
 }
